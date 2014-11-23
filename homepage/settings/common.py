1e612a81a442284e80c73596f7ace8b7f2ba678c
Taylor <taylor@taylor-S400CA.(none)>
2014-11-23 11:52:51 -0600

    no social auth

--- homepage/settings/common.py
+++ homepage/settings/common.py
@@ -215,11 +215,11 @@ THIRD_PARTY_APPS = (
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    'allauth.socialaccount.providers.facebook',
-    'allauth.socialaccount.providers.google',
-    'allauth.socialaccount.providers.linkedin',
-    'allauth.socialaccount.providers.linkedin_oauth2',
-    'allauth.socialaccount.providers.twitter',
+    # 'allauth.socialaccount.providers.facebook',
+    # 'allauth.socialaccount.providers.google',
+    # 'allauth.socialaccount.providers.linkedin',
+    # 'allauth.socialaccount.providers.linkedin_oauth2',
+    # 'allauth.socialaccount.providers.twitter',
 
 
 )
